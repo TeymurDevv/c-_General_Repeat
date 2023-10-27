eb50a23b69e6b0b6f4bfa2d0f6c3ef962758fcdc
TeymurDevv <transmissiyaservis@gmail.com>
2023-10-27 21:58:11 +0400

    Task 2 completed

--- C# General Repeat/Program.cs
+++ C# General Repeat/Program.cs
@@ -16,3 +16,24 @@ while (true)
     }
 }
 #endregion
+#region task2
+Console.WriteLine(" 3 reqemli ededi daxil edin");
+int num2 = int.Parse(Console.ReadLine());
+string str2 = num2.ToString();
+while (true)
+{
+
+
+
+    if (str2.Length == 3)
+    {
+        Console.WriteLine(str2 + str2);
+        break;
+    }
+    else
+    {
+        Console.WriteLine("Yanlış formatda eded daxil etdiniz. Yeniden daxil edin.");
+        str2 = Console.ReadLine();
+    }
+}
+#endregion
